2fb630cec1304cb7163600b48943b2131c1867d0
gisellegoiz <62625308+gisellegoiz@users.noreply.github.com>
2024-05-07 12:41:15 -0300

    Update input_postgre.py

--- input_postgre.py
+++ input_postgre.py
@@ -3,27 +3,30 @@ import pandas as pd
 from io import StringIO
 
 dbname = 'postgres'
-host = 'localhost'
+host = '192.168.1.21'
 user = 'postgres'
 password = '1401'
 port = '5432'
 
 postgres_str = f'postgresql://{user}:{password}@{host}:{port}/{dbname}'
-cnx = create_engine(postgres_str)
+engine = create_engine(postgres_str)
 
-data = {
+data_eMBB = {
+    'slice_id': ['bb5e3eed-9fd7-491b-a49d-f5ed58b8f460'],
+    'service_type': ['eMBB'],
+    'ns_id': ['203e09eb-5d6f-443c-bb1f-9ff0fb4d30d9'],
+    'vnf_id': ['be8de8c3-73f4-43ba-a3ce-a5f7dcef9512'],
+    'vnfd_id': ['83d40159-e8e8-4452-ad8e-391d10f1cb69'],
+    'vdu_id': ['ubuntu_slice-VM'],
     'num-virtual-cpu': ['4'],
     'virtual-memory-size': ['2'],
-    'vnfd_id': ['83d40159-e8e8-4452-ad8e-391d10f1cb69'],
-    'vnf_id': ['d99f8f96-7a65-4786-9371-8da254bbdde8'],
-    'ns_id': ['07adf4c2-a446-4b1e-9f71-7da67c748be0'],
-    'netslice_id': ['38c7aa24-a1d5-41fa-af3b-729de60682dd'],
-    'Service_Type': ['eMBB']
+
 
 }
+df_eMBB = pd.DataFrame(data_eMBB)
+print(df_eMBB)
 
-df = pd.DataFrame(data)
-print(df)
+df_eMBB.to_sql('slice_embb', con=engine, if_exists='append', index=False)
 
+engine.dispose()
 
-df.to_sql('slice_vm1', con=cnx, if_exists='append', index=False)
