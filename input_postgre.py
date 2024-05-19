fc8dc4d8c1df4c7628e80b13d1d3f10ce4a1e9f6
gisellegoiz <62625308+gisellegoiz@users.noreply.github.com>
2024-05-19 02:14:34 -0300

    Update input_postgre.py

--- input_postgre.py
+++ input_postgre.py
@@ -15,7 +15,7 @@ data_eMBB = {
     'slice_id': ['557c6d51-3b04-42e4-bbf5-c836abcc0ea3'],
     'service_type': ['eMBB'],
     'ns_id': ['dc9aeb25-c285-43c4-8560-30fcae1be71b'],
-    'vnf_id': ['51cd1af8-56e8-4bc1-8b68-8dc47b228cff,'],
+    'vnf_id': ['51cd1af8-56e8-4bc1-8b68-8dc47b228cff'],
     'vnfd_id': ['936edd48-0ff5-49fc-bcd4-7d2b56415172'],
     'vdu_id': ['ubuntu_slice-VM'],
     'num_virtual_cpu': ['2'],
